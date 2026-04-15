3e1162f19133e58f10d94953d60d02d763942c3c
DevendraJais <devendra7jaiswal@gmail.com>
2026-04-15 11:42:03 +0530

    add gallery

--- frontend/src/module/delivery/pages/auth/SignupStep2.jsx
+++ frontend/src/module/delivery/pages/auth/SignupStep2.jsx
@@ -224,7 +224,6 @@ export default function SignupStep2() {
                 type="file"
                 className="hidden"
                 accept="image/*"
-                capture="environment"
                 onChange={(e) => {
                   const selectedFile = e.target.files[0]
                   if (selectedFile) {
